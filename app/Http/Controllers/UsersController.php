bdd45d4a3c3bfbb659f0b829b8905fc065368f79
cherish <1356660191@qq.com>
2018-12-30 21:51:56 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,14 @@ class UsersController extends Controller
     {
         return view('users.show',compact('user'));
     }
+
+    public function store(Request $reuqest)
+    {
+        $this->validate($reuqest, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
