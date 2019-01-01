83314a8902181d09fe5e26227fc71624e31159f0
vagrant <vagrant@homestead>
2019-01-01 06:11:19 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -11,7 +12,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except'=>['show', 'create', 'store']
+            'except'=>['show', 'create', 'store' , 'index']
         ]);
     }
 
@@ -73,8 +74,14 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::paginate(10);
-
         return view('users.index', compact('users'));
     }
 
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
 }
